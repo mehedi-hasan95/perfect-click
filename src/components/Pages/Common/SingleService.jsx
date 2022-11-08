a0c66ed1fe46249c001b500a8fb90df313e87a39
mehedititu <mh.me.mehedi@gmail.com>
2022-11-08 11:07:05 +0600

    Gallery page done

--- src/components/Pages/Common/SingleService.jsx
+++ src/components/Pages/Common/SingleService.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const SingleService = ({ serv }) => {
     const { img, title, price, description } = serv;
     return (
-        <div>
+        <div className='shadow-lg shadow-gray-500/50'>
             <img src={img} alt="" className="object-cover object-center w-full rounded-t-md h-72 bg-gray-500" />
             <div className="flex flex-col justify-between p-6 space-y-8">
                 <div className="space-y-2">
                     <h2 className="text-3xl font-semibold tracking-wide text-black">{title}</h2>
-                    <p className="text-black">{description}</p>
+                    <p className="text-black">{description.length < 100 ? description : description.slice(0, 100) + '...'}</p>
                     <h3>{price}</h3>
                 </div>
                 <button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-violet-400 text-gray-900">Details</button>
