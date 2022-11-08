853c8a3e69319e5206ab7ada338f0c8a63673fb0
mehedititu <mh.me.mehedi@gmail.com>
2022-11-08 17:56:42 +0600

    Add data from mongodb

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import SingleService from '../Common/SingleService';
 const Home = () => {
     const [service, setService] = useState([]);
     useEffect( () => {
-        fetch(`services.json`)
+        fetch(`http://localhost:5000/services`)
         .then(res => res.json())
         .then(data => setService(data))
     },[])
