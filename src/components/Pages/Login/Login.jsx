ea1c4a8ae522c8e842ffd95d02ff99c489765b5e
mehedititu <mh.me.mehedi@gmail.com>
2022-11-09 19:17:05 +0600

    Add some bonus Part

--- src/components/Pages/Login/Login.jsx
+++ src/components/Pages/Login/Login.jsx
@@ -3,7 +3,7 @@ import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../Context/AuthProvider';
 
 const Login = () => {
-    const { loginUserWithEmail } = useContext(AuthContext);
+    const { loginUserWithEmail, loading } = useContext(AuthContext);
     const navigate = useNavigate();
     const location = useLocation();
     const from = location.state?.from?.pathname || "/";
@@ -30,6 +30,19 @@ const Login = () => {
         
     }
 
+    // Loading the spinner
+    if (loading) {
+        return <>
+            <div className="container mx-auto flex flex-col m-8 rounded shadow-md w-60 sm:w-80 animate-pulse h-96">
+                <div className="h-48 rounded-t bg-gray-700"></div>
+                <div className="flex-1 px-4 py-8 space-y-4 sm:p-8 bg-gray-900">
+                    <div className="w-full h-6 rounded bg-gray-700"></div>
+                    <div className="w-full h-6 rounded bg-gray-700"></div>
+                    <div className="w-3/4 h-6 rounded bg-gray-700"></div>
+                </div>
+            </div>
+        </>
+    }
 
     
     return (
